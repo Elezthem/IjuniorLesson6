c4f71bc96cbf2b8234cfc4b878b8f5d4d63be4c3
HemyDev <97851451+Elezthem@users.noreply.github.com>
2023-08-25 22:59:44 +0300

    Add files via upload

---
+++ Ijunior/Program.cs
@@ -0,0 +1,23 @@
+﻿string s1 = "Каждый ";
+string s2 = "охотник ";
+string s3 = "желает ";
+string s4 = "знать, ";
+string s5 = "где ";
+string s6 = "сидит ";
+string s7 = "фазан.";
+
+Console.Write(s1);
+Console.Write(s2);
+Console.Write(s3);
+Console.Write(s4);
+Console.Write(s5);
+Console.Write(s6);
+Console.WriteLine(s7);
+
+string concatString = s1 + "" + s2 + "" + s3 + "" + s4 + "" + s5 + "" + s6 + "" + s7 + "";
+
+Console.WriteLine(concatString);
+
+string interpolatedString = $"{s1}{s2}{s3}{s4}{s5}{s6}{s7}";
+
+Console.WriteLine(interpolatedString);
